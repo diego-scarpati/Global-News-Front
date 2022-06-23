591f8d1c44b25a74367630612df2841db405634d
imhmariano <imhmariano@gmail.com>
2022-06-23 19:08:04 -0300

    Mariano

--- Global-News/App.js
+++ Global-News/App.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import backgroundImg from './assets/background-startScreen-02.png'
-=======
 // import { StyleSheet, Text, View } from "react-native";
 import StartScreen from "./components/StartScreen";
 import Login from "./components/Login"
 import { NativeRouter } from "react-router-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
->>>>>>> 62543a71b935c33db804b4a3f2c31f1a425fecf8
 
 export default function App() {
   const Stack = createStackNavigator();

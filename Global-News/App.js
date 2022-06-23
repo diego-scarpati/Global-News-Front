74c8cbb9a7a2318fe0c5759fa5441cf34b9fb334
agustincw <agustincw@gmail.com>
2022-06-23 18:47:09 -0300

    StartScreen

--- Global-News/App.js
+++ Global-News/App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from "react-native";
+import StartScreen from "./components/StartScreen";
+import Login from "./components/Login"
+import { NativeRouter } from "react-router-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={StartScreen}
+      />
+        <Stack.Screen
+        name="Login"
+        component={Login}
+      />
+         {/* <Stack.Screen
+        name="Register"
+        component={Register}
+      /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
---
+++ Global-News/components/AppBar.jsx
@@ -0,0 +1,6 @@
+// logo - login ->nombre+foto
+
+import React from "react"
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+

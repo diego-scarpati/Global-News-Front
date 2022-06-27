c8b66a9198107769605c1ace87cf1ec09176263d
agustincw <agustincw@gmail.com>
2022-06-27 16:21:42 -0300

    sin cambios

--- Global-News/App.js
+++ Global-News/App.js
@@ -1,20 +1,11 @@
 // import { StyleSheet, Text, View } from "react-native";
-
-
-
-
-import MyProfile from "./components/MyProfile/MyProfile"
-
-import License from "./components/License/License"
-import { NativeRouter } from "react-router-native";
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import StartScreen from "./components/StartScreen";
 import Login from "./components/Login"
 import Register from "./components/Register"
-import HomeScreen2 from './components/Home/HomeScreen2'
-
+import License from "./components/License/License"
+import { NativeRouter } from "react-router-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -24,17 +15,17 @@ export default function App() {
       <Stack.Screen name="Home" component={StartScreen}/>
       <Stack.Screen name="Login" component={Login}/>
       <Stack.Screen name="Register" component={Register}/>
-
-      <Stack.Screen name="MyProfile" component={MyProfile}/>
-
-<Stack.Screen name="License" component={License}/>
-
-      <Stack.Screen name ="HomeScreen" component={HomeScreen2}/>
-
-
+      <Stack.Screen name="License" component={License}/>
       </Stack.Navigator>
     </NavigationContainer>
-    
   );
 }
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });

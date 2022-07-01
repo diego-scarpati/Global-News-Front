21a9417232ef025d8e8677bf3108acb96f12794a
Malvarez88 <marianoalvarez66@gmail.com>
2022-07-01 13:02:10 -0300

    licencias

--- Global-News/Views/RRHH/RrHh.jsx
+++ Global-News/Views/RRHH/RrHh.jsx
@@ -1,7 +1,15 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { StyleSheet, Text, View, SafeAreaView, SectionList, StatusBar} from "react-native";
-import { rrhhReviewLicense } from "../../store/license";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  SectionList,
+  StatusBar,
+  Button
+} from "react-native";
+import { rrhhReviewLicense, rrhhChangeLicenseStatus } from "../../store/license";
 
 export default function RrHh() {
   const dispatch = useDispatch();
@@ -13,18 +21,45 @@ export default function RrHh() {
     dispatch(rrhhReviewLicense());
   }, []);
 
+  const handleApprove = (licenceId) => {
+    console.log("licencia aprobada", licenceId);
+    dispatch(rrhhChangeLicenseStatus( {id: licenceId, HRApproval: 'approved'}))
+    dispatch(rrhhReviewLicense());
+  }
+
+  const handleReject = (licenceId) => {
+    console.log("licencia rechazada", licenceId);
+    dispatch(rrhhChangeLicenseStatus( {id: licenceId, HRApproval: 'rejected'}))
+    dispatch(rrhhReviewLicense());
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <SectionList
         sections={[{ title: "Licencias", data: licencias }]}
         renderItem={({ item }) => (
           <View style={styles.row}>
-          
-            <Text>Solicitante: {item.user?.firstName} {item.user?.lastName}</Text>
+            <Text>
+              Solicitante: {item.user?.firstName} {item.user?.lastName}
+            </Text>
             <Text>Tipo de licencia: {item.type}</Text>
             <Text>Inicio: {item.startDate}</Text>
             <Text>Fin: {item.endDate}</Text>
-            <Text>Observaciones:{item.observations}</Text>
+            <Text>Observaciones: {item.observations}</Text>
+            <Text>Estado RRHH: {item.HRApproval}</Text>
+            {item.HRApproval === 'pending' &&
+            <View style={styles.buttomView}>
+              <Button
+                style={styles.button}
+                title="Aprobar"
+                onPress={()=>handleApprove(item.id)}
+              ></Button>
+              <Button
+                style={styles.button}
+                title="Rechazar"
+                onPress={()=>handleReject(item.id)}
+              ></Button>
+            </View>}
           </View>
         )}
         // renderSectionHeader={({ section }) => (
@@ -43,6 +78,11 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: StatusBar.currentHeight,
     marginHorizontal: 16,
+    justifyContent: "center",
+    alignContent: "center",
+    padding: 5,
+    margin: 5,
+   
   },
   item: {
     backgroundColor: "#f9c2ff",
@@ -55,10 +95,24 @@ const styles = StyleSheet.create({
   row: {
     paddingHorizontal: 20,
     paddingVertical: 10,
+    borderColor: "#0073b7",
+    borderWidth: 2,
+    margin: 2,
+    borderRadius: 5,
   },
   sectionHeader: {
     backgroundColor: "#efefef",
     paddingHorizontal: 20,
     paddingVertical: 10,
   },
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10,
+    padding: 10
+  },
+  buttomView: {
+    margin: 10
+  }
 });
--- Global-News/store/license.js
+++ Global-News/store/license.js
@@ -1,40 +1,80 @@
 import axios from "axios";
-import { createReducer, createAsyncThunk, } from "@reduxjs/toolkit";
+import { createReducer, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const sendLicenseRequest = createAsyncThunk("LICENSE", async (data, thunkAPI)=>{
-    try{
-        const { user } = thunkAPI.getState();
-        const info = {data, user}
-        const licencia = await axios.post("http://localhost:3001/api/workLicenses/addLicense", {info} )
-        return licencia.data
-    }catch(error){console.log(error)}
-});
+export const sendLicenseRequest = createAsyncThunk(
+  "LICENSE",
+  async (data, thunkAPI) => {
+    try {
+      const { user } = thunkAPI.getState();
+      const info = { data, user };
+      const licencia = await axios.post(
+        "http://localhost:3001/api/workLicenses/addLicense",
+        { info }
+      );
+      return licencia.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
-export const sendHistoyLicensesRequest = createAsyncThunk("HISTORY_LICENCES", async (data,thunkAPI)=>{
-    try{
-        const { user } = thunkAPI.getState();
-        const licencias = await axios.get(`http://localhost:3001/api/workLicenses/user/${user.id}`)
-        return licencias.data 
-    }catch(error){console.log(error)}
-});
+export const sendHistoyLicensesRequest = createAsyncThunk(
+  "HISTORY_LICENCES",
+  async (data, thunkAPI) => {
+    try {
+      const { user } = thunkAPI.getState();
+      const licencias = await axios.get(
+        `http://localhost:3001/api/workLicenses/user/${user.id}`
+      );
+      return licencias.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
-export const rrhhReviewLicense = createAsyncThunk("RRHH_REVIEW_LICENCE", async ()=> {
-    try{
-        const licences = await axios.get("http://localhost:3001/api/workLicenses/")
-        return licences.data
-    }catch(error){console.log(error)}
-})
+export const rrhhReviewLicense = createAsyncThunk(
+  "RRHH_REVIEW_LICENCE",
+  async () => {
+    try {
+      const licences = await axios.get(
+        "http://localhost:3001/api/workLicenses/"
+      );
+      return licences.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+); //trae las licencias
 
-const licenseReducer = createReducer({}, {
-    [sendLicenseRequest.fulfilled]: (state,action)=>action.payload,
-    [sendLicenseRequest.rejected]: (state,action)=>action.payload,
+export const rrhhChangeLicenseStatus = createAsyncThunk(
+  "RRHH_CHANGE_STATUS_LICENCE",
+  async (data, thunkAPI) => {
+  console.log("🚀 ~ file: license.js ~ line 53 ~ data", data)
+    try {
+      const licences = await axios.put(`http://localhost:3001/api/workLicenses/${data.id}`,data)
+      return licences.data
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
-    [sendHistoyLicensesRequest.fulfilled]: (state,action)=>action.payload,
-    [sendHistoyLicensesRequest.rejected]: (state,action)=>action.payload,
+const licenseReducer = createReducer(
+  {},
+  {
+    [sendLicenseRequest.fulfilled]: (state, action) => action.payload,
+    [sendLicenseRequest.rejected]: (state, action) => action.payload,
 
+    [sendHistoyLicensesRequest.fulfilled]: (state, action) => action.payload,
+    [sendHistoyLicensesRequest.rejected]: (state, action) => action.payload,
 
-    [rrhhReviewLicense.fulfilled]: (state,action)=>action.payload,
-    [rrhhReviewLicense.rejected]: (state,action)=>action.payload,
-});
+    [rrhhReviewLicense.fulfilled]: (state, action) => action.payload,
+    [rrhhReviewLicense.rejected]: (state, action) => action.payload,
 
-export default licenseReducer
+    [rrhhChangeLicenseStatus.fulfilled]: (state, action) => action.payload,
+    [rrhhChangeLicenseStatus.rejected]: (state, action) => action.payload,
+  }
+);
+
+export default licenseReducer;

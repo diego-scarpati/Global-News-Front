b2b376398223e40f19e8579bc41496e7e20e8b48
imhmariano <imhmariano@gmail.com>
2022-07-04 15:12:29 -0300

    Mariano

---
+++ Global-News/Views/Manager/ManagerHomeScreen.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { StyleSheet, View, ScrollView } from "react-native";
+
+import HomeButton from "../HomeScreen/components/HomeButtons";
+
+export default function ManagerHomeScreen({ navigation }) {
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.body}>
+        <HomeButton
+          text="Jefe Solicitud de Licencias"
+          onPress={() => navigation.navigate("Jefe Solicitud de Licencias")}
+        />
+       
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: "#f89d1e",
+    height: 1000,
+    alignItems: "center",
+  },
+});
---
+++ Global-News/Views/Manager/ManagerLicensesRequest.jsx
@@ -0,0 +1,120 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  SectionList,
+  StatusBar,
+  Button
+} from "react-native";
+import { rrhhReviewLicense, rrhhChangeLicenseStatus } from "../../store/license";
+
+export default function ManagerLicencesRequest() {
+  const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.user);
+  const licencias = useSelector((state) => state.license);
+
+  useEffect(() => {
+    dispatch(rrhhReviewLicense());
+  }, []);
+
+  const handleApprove = (licenceId) => {
+    dispatch(rrhhChangeLicenseStatus( {id: licenceId, bossApproval: 'approved'}))
+    dispatch(rrhhReviewLicense());
+  }
+
+  const handleReject = (licenceId) => {
+    dispatch(rrhhChangeLicenseStatus( {id: licenceId, bossApproval: 'rejected'}))
+    dispatch(rrhhReviewLicense());
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+    <Text style={styles.mainText}>Licencias</Text>
+      <SectionList
+        sections={[{ title: "Licencias", data: licencias }]}
+        renderItem={({ item }) => (  
+            item.positionId !== 4      
+          &&<View style={styles.row}>
+            <Text style={styles.text}>
+              Solicitante: {item.user?.firstName} {item.user?.lastName}
+            </Text>
+            <Text>Tipo de licencia: {item.type}</Text>
+            <Text>Inicio: {item.startDate}</Text>
+            <Text>Fin: {item.endDate}</Text>
+            <Text>Observaciones: {item.observations}</Text>
+            <Text>Estado Jefe: {item.bossApproval}</Text>
+            <Text>Estado RRHH: {item.HRApproval}</Text>
+            {item.bossApproval === 'pending' &&
+            <View style={styles.buttomView}>
+              <Button
+                style={styles.button}
+                title="Aprobar"
+                onPress={()=>handleApprove(item.id)}
+              ></Button>
+              <Button
+                style={styles.button}
+                title="Rechazar"
+                onPress={()=>handleReject(item.id)}
+              ></Button>
+            </View>}
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16,
+    justifyContent: "center",
+    alignContent: "center",
+    padding: 5,
+    margin: 5,
+   
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+  },
+  title: {
+    fontSize: 24,
+  },
+  row: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderColor: "#0073b7",
+    borderWidth: 2,
+    margin: 2,
+    borderRadius: 5,
+  },
+  sectionHeader: {
+    backgroundColor: "#efefef",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10,
+    padding: 10
+  },
+  buttomView: {
+    margin: 10
+  },
+  text:{
+    fontSize: 20
+  },
+  mainText:{
+    fontSize: 30
+  }
+});

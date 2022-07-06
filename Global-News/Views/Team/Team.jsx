97389475b8e3ec1ab5053b3694ef27833042d600
Maximiliano Minutillo <maximilminu@gmail.com>
2022-07-06 13:11:46 -0300

    maxi

--- Global-News/Views/Team/Team.jsx
+++ Global-News/Views/Team/Team.jsx
@@ -1,44 +1,45 @@
-import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Table, Row, Rows } from 'react-native-table-component';
+import React, { Component, useEffect } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { DataTable } from "react-native-paper";
+import { searchAllUsers } from "../../store/user";
+import { useSelector, useDispatch } from "react-redux";
+
+export default function Team() {
+  const dispatch = useDispatch()
+  const users = useSelector((state) => state.user)
+
+  useEffect(() => {
+    dispatch(searchAllUsers())
+  }, []);
 
 
-export default class Team extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tableHead: ['Nombre', 'Apellido', 'Email','Cargo', 'Online'],
-      tableData: [
-      ['Mariano', 'Imhoff', 'imhmariano@gmail.com','User','Si'],
-      ['Solomeo', 'Paredes', 'solomeo.p@gmail.com','User', 'No'],
-      ['Armando', 'Casas', 'armando.c@gmail.com','User', 'No'],
-      ['Elba', 'Tracio', 'elba.t@gmail.com','User', 'Si']]
-    }
-  }
 
-  render() {
-    const state = this.state;
-    return (
-      
-      
-      <View style={styles.container}>
-        <Table borderStyle={styles.border}>
-          <Row data={state.tableHead} style={styles.head} textStyle={{color:'#fff', fontSize:15}}/>
-          <Rows data={state.tableData} textStyle={styles.text}/>
-        </Table>
-      </View>
-      
-      
-    )
-  }
+
+  return (
+    users.lenght? users.map( user => (
+    <View style={styles.container}>
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>Name</DataTable.Title>
+          <DataTable.Title>Email</DataTable.Title>
+          <DataTable.Title numeric>Age</DataTable.Title>
+        </DataTable.Header>
+
+        <DataTable.Row>
+          <DataTable.Cell>{user.firstName}</DataTable.Cell>
+          <DataTable.Cell>john@kindacode.com</DataTable.Cell>
+          <DataTable.Cell numeric>33</DataTable.Cell>
+        </DataTable.Row>
+      </DataTable>
+    </View>
+   )):
+   <Text>NOP</Text>
+  )
 }
 
 const styles = StyleSheet.create({
-  border:{borderWidth: 2, borderColor: '#0073b7'},
-  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
-  head: { height: 40, textAlign: 'center', backgroundColor:'#0073b7'},
-  text: { margin: 10,textAlign: 'center', fontSize:10 },
-  
-
+  container: {
+    paddingTop: 100,
+    paddingHorizontal: 30,
+  },
 });
-
